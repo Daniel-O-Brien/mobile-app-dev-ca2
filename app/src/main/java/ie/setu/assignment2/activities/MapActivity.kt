ed09d7cbab8e7ffb837bdc04886bccfbe87806d1
Daniel O'Brien <20101948@setu.ie>
2025-12-19 14:20:54 +0000

    added game stores placemark

--- app/src/main/java/ie/setu/assignment2/activities/MapActivity.kt
+++ app/src/main/java/ie/setu/assignment2/activities/MapActivity.kt
@@ -1,7 +1,13 @@
 package ie.setu.assignment2.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -11,16 +17,38 @@ import com.google.android.gms.maps.model.MarkerOptions
 import ie.setu.assignment2.R
 import ie.setu.assignment2.databinding.ActivityMapBinding
 
+//https://developers.google.com/android/reference/com/google/android/gms/location/FusedLocationProviderClient
+
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private val cexStores = listOf(
+        LatLng(52.338464941659275, -6.461739201940897),
+        LatLng(52.26023569713962, -7.1116102756176796)
+    )
+
+    private val smithsToysStores = listOf(
+        LatLng(52.249689898304005, -7.118675816142929)
+    )
+
+    private val kenBlackStores = listOf(
+        LatLng(52.32625698224165, -6.489213593842555)
+    )
+
+    private val otherStores = listOf(
+        LatLng(52.33696998042763, -6.46065961635431)
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
@@ -37,12 +65,62 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+//    override fun onMapReady(googleMap: GoogleMap) {
+//        mMap = googleMap
+//
+//        // Add a marker in Sydney and move the camera
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Check for location permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Request permissions
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            return
+        }
+
+        // Enable MyLocation Layer of Google Map
+        mMap.isMyLocationEnabled = true
+
+        // Get the last known location
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            if (location != null) {
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+//                mMap.addMarker(MarkerOptions().position(currentLatLng).title("You are here"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+            }
+        }
+
+        for (store in cexStores) {
+            mMap.addMarker(MarkerOptions().position(store).title("CEX Store"))
+        }
+        for (store in smithsToysStores) {
+            mMap.addMarker(MarkerOptions().position(store).title("Smith Toys Store"))
+        }
+        for (store in kenBlackStores) {
+            mMap.addMarker(MarkerOptions().position(store).title("Keb Black Store"))
+        }
+        for (store in otherStores) {
+            mMap.addMarker(MarkerOptions().position(store).title("Game Store"))
+        }
+    }
+
+    // Handle permission request response
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // Permission granted, refresh the map
+                onMapReady(mMap)
+            } else {
+                // Permission denied, handle accordingly
+            }
+        }
     }
 }
